0a52a953cc6fdd6b477800cc0c424417dc107af4
Johnny <jcarreiropublicidade@gmail.com>
2022-01-12 15:47:59 -0300

    feat: Add font and color theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,34 +1,43 @@
 const defaultTheme = {
-  color:{
+  colors:{
       gray_100: '#00000F',
-      gray_200: '#343940',
-      gray_300: '#697380',
-      gray_400: '#9DACBF',
-      gray_500: '#D1E5FF',
-      gray_600: '#DAEAFF',
-      gray_700: '#E4EFFF',
-      gray_800: '#EDF5FF',
-      gray_900: '#F6FAFF',
-      gray_1000: '#F7FAFF',
+      gray_200: '#121820',
+      gray_300: '#253040',
+      gray_400: '#374860',
+      gray_500: '#4a5f80',
+      gray_600: '#6e7f99',
+      gray_700: '#929fb3',
+      gray_900: '#b7bfcc',
+      gray_1000: '#dbdfe6',
+      white: '#F7FAFF',
 
       main:'#7029E1',
-      primary: '#1279FF',
-      secondary: '#F78D1E',
+      primary: '#fdac53',
+      secondary: '#4791ff',
 
-      alert: '#6BA2F6',
-      danger: '#d31e45',
-      success:'#45FF29',
+      alert: '#b07028',
+      danger: '#ff6177',
+      success:'#6cfd7e',
 
   },
   texts:{
-    main_title:'900 3rem/4.25rem Roboto, sans-serif',//h1
-    title:'700 2rem/3.5rem Roboto, sans-serif',//h2
-    sub_title:'700 1.5rem/3rem Roboto, sans-serif',//h3
-    main_text:'400 1rem/1.5rem Roboto, sans-serif',//p, a ...
-    main_strong:'700 1rem/1.5rem Roboto, sans-serif',//strong
-    small_text:'300 0.875rem/1.3125rem Roboto, sans-serif'//p, a, small ..
+    main_title:'700 3rem/4.25rem Montserrat, sans-serif',//h1
+    title:'700 2rem/3.5rem Montserrat, sans-serif',//h2
+    sub_title:'600 1.5rem/3rem Montserrat, sans-serif',//h3
+    main_text:'400 1rem/1.5rem Montserrat, sans-serif',//p, a ...
+    main_strong:'600 1rem/1.5rem Montserrat, sans-serif',//strong
+    small_text:'300 0.875rem/1.3125rem Montserrat, sans-serif'//p, a, small ..
   }
 }
 
 export type ThemeType = typeof defaultTheme
 export default defaultTheme
+
+// brown: '#b07028',
+// light: '#7c2eca',
+// pruple: '#7131b0',
+// main: '#fdac53',
+// secondary: '#4791ff',
+// dark: '#205bb2',
+// light: '#ccc524',
+// dark: '#b2ad29',
